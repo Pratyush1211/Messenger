54fb784f4b3b9bd1250459ae8f739d3e2c7697d7
Shivansh Goel <shivanshgoel21@gmail.com>
2021-09-20 16:56:49 +0530

    add required modules for backend integration

---
+++ services/localstorage.js/read.js
@@ -0,0 +1,17 @@
+  
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+async function read(key){
+
+    try{
+        const value = await AsyncStorage.getItem(key)
+
+        return value
+
+    } catch(err){
+        return err
+    }
+
+}
+
+export default read
